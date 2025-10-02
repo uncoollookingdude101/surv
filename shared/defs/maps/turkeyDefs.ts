702d6d724a397049fc70f68fe483a0160e8cfd15
Hexevillius <perhac3@gmail.com>
2025-10-02 15:53:08 +0200

    feat: turkey defs

--- shared/defs/maps/turkeyDefs.ts
+++ shared/defs/maps/turkeyDefs.ts
@@ -19,8 +19,89 @@ const mapDef: PartialMapDef = {
             { name: "xp_drop_02", channel: "sfx" },
             { name: "pumpkin_break_01", channel: "sfx" },
         ],
+        atlases: ["main", "gradient", "loadout", "shared", "woods"],
+    },
+    biome: {
+         colors: {
+            background: 0x474D4A,
+            water: 0x707B76,
+            waterRipple: 0x7E8984,
+            beach: 0xCC975B,
+            riverbank: 0xBD5C21,
+            grass: 0xA08B2F,
+            underground: 0x1b0d03,
+            playerSubmerge: 0x2b8ca4,
+            playerGhillie: 0xA48E2E,
+        },
+    },
+        mapGen: {
+        densitySpawns: [
+            {
+                stone_01: 250,
+                barrel_01: 76,
+                silo_01: 9,
+                crate_01: 50,
+                crate_02: 4,
+                crate_03: 8,
+                cache_03tr: 100, // leaf pile turkey
+                cache_06: 12, // cherry bush
+                tree_07: 20,
+                tree_08: 170,
+                tree_08b: 4,
+                hedgehog_01: 24,
+                container_01: 5,
+                container_02: 5,
+                container_03: 5,
+                container_04: 5,
+                shack_01: 7,
+                outhouse_01: 7,
+                outhouse_02: 2, // fire axe outhouse
+                loot_tier_1: 24,
+                loot_tier_beach: 4,
+                squash_01: 24,
+                woodpile_02: 4,
+            },
+        ],
+            fixedSpawns: [
+            {
+                // small is spawn count for solos and duos, large is spawn count for squads
+                warehouse_01: 3,
+                house_red_01: { small: 3, large: 4 },
+                house_red_02: { small: 3, large: 4 },
+                barn_01: { small: 1, large: 3 },
+                barn_02: 1,
+                hut_01: 1,
+                hut_02: { odds: 0.05 }, // spas hut
+                hut_03: { odds: 0.05 }, // scout hut
+                shack_03a: 2,
+                shack_03b: { small: 2, large: 3 },
+                greenhouse_01: 1,
+                cache_01: 1,
+                cache_02: 1, // mosin tree
+                cache_07: 1,
+                bunker_structure_01: { odds: 0.05 },
+                bunker_structure_02: 1,
+                bunker_structure_03: 1,
+                bunker_structure_04: 1,
+                bunker_structure_05: 1,
+                warehouse_complex_01: 1,
+                chest_01: 1,
+                chest_03tr: { odds: 0.2 },
+                mil_crate_02: { odds: 0.25 },
+                tree_02: 4,
+                stone_04: 1,
+                club_complex_01: 1,
+            },
+        ],
+        spawnReplacements: [
+            {
+                tree_01: "tree_08",
+                stone_03: "stone_03tr",
+            },
+        ],
     },
     gameMode: { turkeyMode: 1 },
+    
 };
 
 export const Turkey = util.mergeDeep({}, Main, mapDef);
