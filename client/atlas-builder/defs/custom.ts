20c435ba22732fc2d50d272044daeb532f04f697
omgaflyingderp <omgaflyingderp@gmail.com>
2025-10-05 19:55:47 +0700

    butterfingers

--- client/atlas-builder/defs/custom.ts
+++ client/atlas-builder/defs/custom.ts
@@ -8,7 +8,7 @@ export const CustomAtlas: AtlasDef = {
         "guns/gun-m79-01.svg",
         "loot/loot-weapon-m79.svg",
         "proj/proj-40mm-01.svg",
-        "guns/gun-lasr-gunuy.svg",
+        "guns/gun-lasr-gun.svg",
         "loot/loot-weapon-lasr-gun.svg",
         "loot/loot-weapon-double-lasr-gun.svg"
     ],
