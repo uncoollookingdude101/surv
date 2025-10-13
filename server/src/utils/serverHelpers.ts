414473a820e066371c5513a1d29798380d615047
leia uwu <leia@tutamail.com>
2025-10-13 15:25:09 -0300

    fix: retry private API fetch requests

--- server/src/utils/serverHelpers.ts
+++ server/src/utils/serverHelpers.ts
@@ -428,9 +428,34 @@ export async function verifyTurnsStile(token: string, ip: string): Promise<boole
     return true;
 }
 
+function fetchWithRetry(input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
+    return new Promise((resolve, reject) => {
+        const tryFetch = (attempts: number) => {
+            fetch(input, init)
+                .then(resolve)
+                .catch((err) => {
+                    if (attempts < 3) {
+                        defaultLogger.warn(`Failed to fetch ${input}, retrying`);
+                        setTimeout(
+                            () => {
+                                tryFetch(++attempts);
+                            },
+                            (attempts + 1) * 1000,
+                        );
+                    } else {
+                        reject(err);
+                    }
+                });
+        };
+
+        tryFetch(0);
+    });
+}
+
 export const apiPrivateRouter = hc<PrivateRouteApp>(
     `${Config.gameServer.apiServerUrl}/private`,
     {
+        fetch: fetchWithRetry,
         headers: {
             "survev-api-key": Config.secrets.SURVEV_API_KEY,
         },
